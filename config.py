da2f54ad528a2a59483b9bf4b481d44dd9e0eea7
divya-csekar <divya.chandra.sekar@west.cmu.edu>
2014-10-24 02:09:25 -0700

    blog server with web login

---
+++ config.py
@@ -0,0 +1,7 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'what-do-you-think-it-is'
+
+OPENID_PROVIDERS = [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	{'name': 'Flickr', 'url': 'https://www.flickr.com/<username>'}]
