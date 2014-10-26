9a1fe9042a76172874fd5e7d2d7f0a68f9b9ab5e
divya-csekar <divya.chandra.sekar@west.cmu.edu>
2014-10-26 05:51:32 -0700

    adding login form

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ SECRET_KEY = 'what-do-you-think-it-is'
 OPENID_PROVIDERS = [
 	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
 	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-	{'name': 'Flickr', 'url': 'https://www.flickr.com/<username>'}]
+	{'name': 'Flickr', 'url': 'https://www.flickr.com/<username>'},
+	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
